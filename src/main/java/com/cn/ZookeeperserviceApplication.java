612fe32549cdc06aaca3090d8590cbd4689f359a
Administrator <13911008254@139.com>
2019-09-16 17:43:22 +0800

    111

---
+++ src/main/java/com/cn/ZookeeperserviceApplication.java
@@ -0,0 +1,17 @@
+package com.cn;
+
+/**
+ * Zookeeper测试
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+
+public class ZookeeperserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperserviceApplication.class, args);
+    }
+
+}
