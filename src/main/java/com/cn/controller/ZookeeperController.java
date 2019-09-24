ba9dd77fe764a7295ea07c43398d8865f8082691
Administrator <13911008254@139.com>
2019-09-24 16:38:05 +0800

    111

--- src/main/java/com/cn/controller/ZookeeperController.java
+++ src/main/java/com/cn/controller/ZookeeperController.java
@@ -19,12 +19,20 @@ public class ZookeeperController {
     public ZookeeperController() {
     }
 
+    /**
+     * 获取分布式锁
+     * @param type
+     * @param znode
+     * @param nodeData
+     * @return
+     */
     @RequestMapping(value = "/create",method=RequestMethod.POST)
     public String create(int type,String znode,String nodeData){
         //znode = "/product-lock-" + znode;
         try {
             //zkClient.createNode(CreateMode.fromFlag(type),znode,nodeData);
-            zkSession.acquireDistributedLock(znode,nodeData);
+            zkSession.acquireDistributedLock(znode,nodeData); // 如果没有成功获取锁，下面的语句将不会执行，程序进入等待状态。
+            System.out.println("成功的获取了分布式锁：－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－－");
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -32,4 +40,17 @@ public class ZookeeperController {
         return znode;
     }
 
+    /**
+     * 释放分布锁
+     */
+    @RequestMapping(value = "/release",method=RequestMethod.POST)
+    public String releaseDistributedLock(String znode) {
+        try {
+            zkSession.releaseDistributedLock(znode);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return znode;
+    }
 }
